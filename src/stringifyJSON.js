17697037ca2030c669027d28d170a5bd3c07e53a
Brian Kilrain <bkilrain@gmail.com>
2016-07-10 13:31:51 -0700

    Rewrite implementation for objects and arrays
    -My original code destroyed the original object. Now I'm using a
    for loop to iterate through the object and only using recursion
    for when there is multidimensional traversing.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,22 +3,44 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) { 
-	if (typeof obj === 'boolean' || typeof obj === 'number') { //checks numbers and booleans
-		return obj.toString();
-	} else if (obj === null) { //checks null
-		return "null";
-	}
-	
-	
-	
-	var keys = Object.keys(obj);
-	var key = keys[0];
-	if (keys.length === 1) {
-		return '{"' + key + '"' + ':"' + obj[key] + '"}';
-	} else {
-		var strung = '{"' + key + '"' + ':"' + obj[key] + '"';
-		delete obj[key];
-		return strung + ',' + stringifyJSON(obj).slice(1);
-	}
+var stringifyJSON = function(obj) {
+
+  var basicType = function(obj) { //stringifies non-object and array data-types
+    if (typeof obj === 'boolean' || typeof obj === 'number') { //checks numbers and booleans
+      return obj.toString();
+    } else if (obj === null) { //checks null
+      return "null";
+    } else if (typeof obj === 'string') { //checks strings
+      return '"' + obj + '"';
+    }
+  };
+
+  if (Array.isArray(obj)) { // for arrays
+    var strungArray = [];
+    for (var i = 0; i < obj.length; i++) {
+      var item = obj[i];
+      if (typeof item !== 'object') {
+        strungArray.push(basicType(item));
+      } else {
+        strungArray.push(stringifyJSON(item));
+      }
+    }
+    return '[' + strungArray.join(',') + ']';
+  }
+
+  if (obj !== null && typeof obj === 'object') { //for objects
+    var keys = Object.keys(obj);
+    var strungArray = [];
+    for (var i = 0; i < keys.length; i++) {
+      var value = obj[keys[i]];
+      var key = keys[i];
+      if (typeof value !== 'object' || value === null) {
+        strungArray.push('"' + key + '":' + basicType(value));
+      } else {
+        strungArray.push('"' + key + '":' + stringifyJSON(value));
+      }
+    }
+    return '{' + strungArray.join(',') + '}';
+  }
+
 };
