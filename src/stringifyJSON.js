3c789ea79dd9cae8e171e091ec18783004e1a339
Brian Kilrain <bkilrain@gmail.com>
2016-07-09 18:12:10 -0700

    Attempt to re-implement JSON.stringify recursively
    -The function is almost complete when an object literal is used
    as an argument. I still need to figure out a good way to add
    an opening curly bracket to the beginning of the stringified JSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,17 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj) { 
+	var keys = Object.keys(obj);
+	var key = keys[0];
+	console.log(keys);
+	if (keys.length === 1) {
+		return '"' + key + '"' + ':' + obj[key] + "}";
+	} else {
+		var strung = '"' + key + '"' + ':' + obj[key]
+		delete obj[key];
+		return strung + ',' + stringifyJSON(obj);
+	}
 };
+
+console.log('finished: ', stringifyJSON({x:2,y:3,z:4}));
