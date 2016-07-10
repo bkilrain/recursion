f9dfae53e6a53610590d4679ce6eca1ddff865b6
Brian Kilrain <bkilrain@gmail.com>
2016-07-10 08:03:44 -0700

    Add number, boolean and null functionality to strigifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,12 +4,20 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) { 
+	if (typeof obj === 'boolean' || typeof obj === 'number') { //checks numbers and booleans
+		return obj.toString();
+	} else if (obj === null) { //checks null
+		return "null";
+	}
+	
+	
+	
 	var keys = Object.keys(obj);
 	var key = keys[0];
 	if (keys.length === 1) {
-		return '{"' + key + '"' + ':' + obj[key] + "}";
+		return '{"' + key + '"' + ':"' + obj[key] + '"}';
 	} else {
-		var strung = '{"' + key + '"' + ':' + obj[key]
+		var strung = '{"' + key + '"' + ':"' + obj[key] + '"';
 		delete obj[key];
 		return strung + ',' + stringifyJSON(obj).slice(1);
 	}
