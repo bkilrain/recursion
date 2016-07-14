a2ce98923057814d2b497c892517aca55e0c7d1c
Brian Kilrain <bkilrain@gmail.com>
2016-07-14 15:10:24 -0700

    Restart parseJSON implementation
    -I did more research on recursive descent parsers and realized
    my first two attempts had holes in the logic. I am following the expanation
    at the blog here: http://wesleytsai.io/2015/06/13/a-json-parser/
    of a JSON parser.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,40 +3,37 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-    var numRegex = /-?\d*\.?\d+/g
-
-//check next grammar pattern
-  var checkGrammar = function(string, start, end) {
-    if (string.indexOf('[', start) >= 0) {
-      return getArray(string, string.indexOf('['), end);
-    } else if (string.search(numRegex) >= 0) {
-      return getNumber(string)
-    }
+  var indx = 0;
+  var char;
   
-
-
+// string traverser
+  var nextCheck = function() {
+    indx++;
+    char = json.charAt(indx);
+    return char;
   }
 
-//call when object start is matched
-  var getObject = function(string) {
-
+  var grammar = function() {
+    if (char === '{') {
+      return getObject();
+    } else if (char === '[') {
+      return getArray();
+    } else if (/[-\d]/.test(char)) {
+      return getNumber; 
+    } else if (char === '"') {
+      return getString();
+    } else if (char === 't' || char === 'f') {
+      return getBoolean();
+    } else if (char === 'n') {
+      return getNull();
+    } else {
+      throw 'invalid input';
+    }
   }
+    
+// functions that return chunks of grammar and update traverser variable
 
-  var getQuotes = function(string) {
-  	
+  var null = function() {
+    
   }
-
-  var getArray = function(string, start, end) {
-    var endIndex = string.lastIndexOf(']');
-    return [checkGrammar(string, start + 1, endIndex - 1)];
-  }
-
-  var getNumber = function(string) { // returns next number in string (don't need start or end args
-    var info = numRegex.exec(string) // because it's built into the regex exec method)
-    var endIndex = numRegex.lastIndex;
-    return Number(info[0]);
-  }
-
-  var test = "[-32.42]"
-  console.log(checkGrammar(test, 0, test.length - 1));
 };
